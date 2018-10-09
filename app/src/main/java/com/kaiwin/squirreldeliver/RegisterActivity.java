8c7f6115131fb4379bbe999d7f90e726cbd46d56
luche <123@qq.com>
2018-10-09 22:45:09 +0800

    adapter register items

--- app/src/main/java/com/kaiwin/squirreldeliver/RegisterActivity.java
+++ app/src/main/java/com/kaiwin/squirreldeliver/RegisterActivity.java
@@ -16,8 +16,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
-import java.util.List;
-
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -75,35 +73,35 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 String phone = phoneEditText.getText().toString().trim();
 
 
-                if (email.isEmpty() || pwd.isEmpty() || pwdRepeated.isEmpty()){
+                if (email.isEmpty() || pwd.isEmpty() || pwdRepeated.isEmpty()) {
                     Toast.makeText(getApplicationContext(), R.string.empty_input, Toast.LENGTH_SHORT).show();
                     return;
-                }else if (!pwd.equals(pwdRepeated)){
+                } else if (!pwd.equals(pwdRepeated)) {
                     Toast.makeText(getApplicationContext(), R.string.password_is_different, Toast.LENGTH_SHORT)
                             .show();
                     return;
-                }else if (pwd.length()<6){
+                } else if (pwd.length() < 6) {
                     Toast.makeText(getApplicationContext(), R.string.password_should_be_at_least_6_characters, Toast.LENGTH_SHORT)
                             .show();
                     return;
                 }
 
 
-                mAuth.createUserWithEmailAndPassword(email,pwd)
-                        .addOnCompleteListener(this,task -> {
-                           if (task.isSuccessful()){
-                               Toast.makeText(getApplicationContext(), R.string.register_success, Toast.LENGTH_SHORT)
-                                       .show();
-                               startActivity(new Intent(this, LoginActivity.class)
-                                       .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)
-                               );
-                               sendEmailVerification(mAuth.getCurrentUser());
-                           }else {
-                               Snackbar.make(btnRegsiter,task.getException().toString(),Snackbar.LENGTH_LONG).show();
-                               new AlertDialog.Builder(this)
-                                       .setTitle("ERROR").setMessage(task.getException().toString())
-                               .show();
-                           }
+                mAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), R.string.register_success, Toast.LENGTH_SHORT)
+                                        .show();
+                                startActivity(new Intent(this, LoginActivity.class)
+                                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)
+                                );
+                                sendEmailVerification(mAuth.getCurrentUser());
+                            } else {
+                                Snackbar.make(btnRegsiter, task.getException().toString(), Snackbar.LENGTH_LONG).show();
+                                new AlertDialog.Builder(this)
+                                        .setTitle("ERROR").setMessage(task.getException().toString())
+                                        .show();
+                            }
                         });
                 break;
             default:
@@ -111,20 +109,19 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
 
 
-
         //Log.i(view.getTag().toString() + "has been pressed.", "Click Event");
     }
 
 
-    private void updateUserInfo(){
+    private void updateUserInfo() {
         UserProfileChangeRequest profileUpdates =
                 new UserProfileChangeRequest.Builder()
-                .setDisplayName("Jane Q. User")
-                .build();
+                        .setDisplayName("Jane Q. User")
+                        .build();
 
     }
 
-    private void sendEmailVerification(FirebaseUser user){
+    private void sendEmailVerification(FirebaseUser user) {
         user.sendEmailVerification().addOnCompleteListener(task -> {
             if (!task.isSuccessful()) {
                 Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT)
