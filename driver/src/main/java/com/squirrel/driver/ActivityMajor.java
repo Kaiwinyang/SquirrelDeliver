fa7fa5f27823e45ad24cd5bcfec373a19534361b
luche <123@qq.com>
2018-11-03 07:26:23 +0800

    Make preparation including application label, fragments, icons, viewpager.

--- driver/src/main/java/com/squirrel/driver/ActivityMajor.java
+++ driver/src/main/java/com/squirrel/driver/ActivityMajor.java
@@ -3,13 +3,28 @@ package com.squirrel.driver;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ActivityMajor extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    FragmentOrdersComplete fragmentOrdersComplete = FragmentOrdersComplete.newInstance("", "");
+    FragmentOrdersInSuspense fragmentOrdersInSuspense = FragmentOrdersInSuspense.newInstance("");
+    FragmentOrdersProcessing fragmentOrdersProcessing = FragmentOrdersProcessing.newInstance("");
+    FragmentSettings fragmentSettings = FragmentSettings.newInstance("");
+
+
+    BottomNavigationView bottomNavigation;
+    ViewPagerAdapter adapter;
+
+    ViewPager viewPager;
+    //MenuItem menuItem;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,27 +33,88 @@ public class ActivityMajor extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_orders_in_suspense:
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_my_orders:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_orders_complete:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2);
+                    return true;
+                case R.id.navigation_sittings:
+                    viewPager.setCurrentItem(3);
                     return true;
             }
             return false;
         }
     };
 
+    private ViewPager.OnPageChangeListener mOnPageChangeListener
+            = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            bottomNavigation.getMenu().getItem(position).setChecked(true);
+        }
+
+
+    };
+
+
+    private void setupViewPager() {
+        viewPager = findViewById(R.id.majorViewPager);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(fragmentOrdersInSuspense)
+                .addFragment(fragmentOrdersProcessing)
+                .addFragment(fragmentOrdersComplete)
+                .addFragment(fragmentSettings);
+
+        viewPager.setOffscreenPageLimit(adapter.getCount());
+        viewPager.addOnPageChangeListener(mOnPageChangeListener);
+        viewPager.setAdapter(adapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_major);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigation = findViewById(R.id.navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setupViewPager();
     }
 
+}
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    public ViewPagerAdapter addFragment(Fragment fragment) {
+        fragments.add(fragment);
+        return this;
+    }
 }
--- driver/src/main/java/com/squirrel/driver/FragmentSettings.java
+++ driver/src/main/java/com/squirrel/driver/FragmentSettings.java
@@ -1,7 +1,5 @@
 package com.squirrel.driver;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,7 +10,6 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FragmentSettings.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link FragmentSettings#newInstance} factory method to
  * create an instance of this fragment.
@@ -27,8 +24,6 @@ public class FragmentSettings extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
-
     public FragmentSettings() {
         // Required empty public constructor
     }
@@ -38,15 +33,15 @@ public class FragmentSettings extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+
      * @return A new instance of fragment FragmentSettings.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentSettings newInstance(String param1, String param2) {
+    public static FragmentSettings newInstance(String param1) {
         FragmentSettings fragment = new FragmentSettings();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -67,42 +62,4 @@ public class FragmentSettings extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment_settings, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
