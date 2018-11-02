d81585fab282c7e32b8e496b70a39df51d6bc511
luche <123@qq.com>
2018-11-03 05:39:59 +0800

    Change module driver's string.xml

--- driver/src/main/java/com/squirrel/driver/ActivityMajor.java
+++ driver/src/main/java/com/squirrel/driver/ActivityMajor.java
@@ -17,13 +17,13 @@ public class ActivityMajor extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_orders_in_suspense:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_my_orders:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_orders_complete:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
